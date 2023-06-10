8d75a03ab66b3337551fafc96340b618af4547ae
kasimovvladlen2006rez@gmail.com <kasimovvladlen2006rez@gmail.com>
2023-06-11 00:57:22 +0500

    + 1 Task закончить делать со строками

---
+++ Collatz/TaskCollatz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collatz
+{
+    // Если число четное, разделите его на два.
+    // Если число нечетное, утроите его и прибавьте единицу.
+    // Если число один, закончите операцию.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Equals("3->10->5->16->8->4->2->1", Kata.Collatz1(3)));
+        }
+    }
+    public class Kata
+    {
+        public static string Collatz1(int n)
+        {
+            List<int> numbers = new List<int> { n };
+            while (n != 1)
+                if (n % 2 == 0)
+                    numbers.Add(n /= 2);
+                else if (n % 2 == 1)
+                    numbers.Add(n = (n * 3) + 1);
+            return string.Join("->", numbers);
+        }
+    }
+}
---
+++ UniqueInOrder/Task19me.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueInOrder
+{
+    internal class Task19me
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Equals("ABCDAB", Kata.UniqueInOrder("AAAABBBCCDAABBB")));
+        }
+    }
+
+    public static class Kata
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            iterable.OfType<T>().ToList();
+            return iterable;
+        }
+        public static int DuplicateCount(string str)
+        {
+            List<char> chars = str.ToLower().ToList();
+            chars.Sort();
+
+            int count = 0;
+
+            for (int i = 0; i < chars.Count; i++)
+            {
+                for (int j = i + 1; j < chars.Count; j++)
+                {
+                    if (chars[i] == chars[j])
+                    {
+                        if (chars[j] == ' ')
+                            break;
+                        while (chars[i] == chars[j])
+                        {
+                            chars.Remove(chars[j]);
+                            chars.Add(' ');
+                        }
+                        count += 1;
+                        j--;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
